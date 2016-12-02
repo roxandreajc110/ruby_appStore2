0cabf4d525682b19915dd07e4b26ec3c54636575
Roxana Jaramillo <roxandreajc110@hotmail.com>
2016-12-01 21:31:08 -0500

    Create session,users

--- config/routes.rb
+++ config/routes.rb
@@ -1,40 +1,5 @@
 Rails.application.routes.draw do
-  get 'purchases/index'
-
-  get 'purchases/create'
-
-  get 'purchases/show'
-
-  get 'purchases/edit'
-
-  get 'purchases/update'
-
-  get 'purchases/destroy'
-
-  get 'categories/index'
-
-  get 'categories/create'
-
-  get 'categories/show'
-
-  get 'categories/edit'
-
-  get 'categories/update'
-
-  get 'categories/destroy'
-
-  get 'applications/index'
-
-  get 'applications/create'
-
-  get 'applications/show'
-
-  get 'applications/edit'
-
-  get 'applications/update'
-
-  get 'applications/destroy'
-
+  
   root to: 'static_pages#welcome'
   devise_for :users, controllers: {
           sessions: 'users/sessions',
